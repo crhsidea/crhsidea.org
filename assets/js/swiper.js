5b09eecc0344699690ae43b139b07f02bf838f88
Kevin G <ge.kevin.a@gmail.com>
2021-05-31 19:51:08 -0500

    Initial commit

---
+++ assets/js/swiper.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper-container", {
+    spaceBetween: -10,
+    speed: 825,
+    parallax: true,
+    autoplay: true,
+    loop: true,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: ".custom-swiper-button-next",
+      prevEl: ".custom-swiper-button-prev",
+    },
+  });
+});
