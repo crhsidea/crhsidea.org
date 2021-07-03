afe2b66dd17ed2d740602199231a928d71381dc3
330i <enactirace@gmail.com>
2021-07-03 07:33:01 -0500

    Revert "the projects section (unfinished)"
    This reverts commit 6f6360dda0ce9fb2c9cf2f4a94323a1bf58c2f42.

--- assets/js/projectsSwiper.js
+++ assets/js/projectsSwiper.js
@@ -1,21 +1,72 @@
-document.querySelectorAll(".projectinfo").forEach(function(projectinfo) {
-  projectinfo.addEventListener("mouseenter", function(event) {
-    let projectnumber = projectinfo.getAttribute('projectnumber');
-    document.querySelector(".project-count-h1").style.opacity = "0";
-    for(let i=1;i<7;i++) {
-      document.querySelector(`.project-img${i}`).classList.remove(`project-img${i}-transition`);
-    }
-    document.querySelector(`.project-img${projectnumber}`).classList.add(`project-img${projectnumber}-transition`);
-  });
-});
-
 document.addEventListener("DOMContentLoaded", () => {
-  if (window.innerWidth < 780) {
-    document.querySelector(".projectinfo").style.width = "80vw";
-    document.querySelector(".projectinfo").style.margin = "0";
-  } else {
-    document.querySelector(".projectinfo").style.width = "40vw";
-    document.querySelector(".projectinfo").style.margin = "0 0 0 40vw";
-  }
-  window.addEventListener("resize", checkWindowSize);
+  new Swiper("#swiper-projects", {
+    freeMode: true,
+    freeModeMomentum: true,
+    freeModeMomentumVelocityRatio: 0.2,
+    parallax: true,
+    loop: false,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    scrollbar: {
+      el: "#projects-swiper-scrollbar",
+      draggable: true,
+    },
+    touchStartPreventDefault: false,
+    on: {
+      afterInit: () => {
+        const scrollThumb = document.querySelector(".swiper-scrollbar-drag");
+        scrollThumb.style.backgroundColor = "var(--punch)";
+      },
+    },
+    breakpoints: {
+      200: {
+        slidesPerView: 1,
+      },
+      700: {
+        slidesPerView: 2,
+      },
+      1000: {
+        slidesPerView: 3,
+      },
+      1536: {
+        slidesPerView: 4,
+      },
+    },
+  });
+
+  const projectCards = document.querySelectorAll(".projects-card");
+
+  const swiperContainer = document.getElementById("swiper-projects");
+
+  swiperContainer.onmousedown = () => {
+    projectCards.forEach((el) => {
+      el.style.transform = "scale(0.9)";
+    });
+  };
+
+  swiperContainer.onmouseup = () => {
+    projectCards.forEach((el) => {
+      el.style.transform = "";
+    });
+  };
+
+  swiperContainer.onmouseleave = () => {
+    projectCards.forEach((el) => {
+      el.style.transform = "";
+    });
+  };
+
+  projectCards.forEach((el) => {
+    el.addEventListener("mouseenter", () => {
+      const paragraph = el.querySelector("p");
+      paragraph.style.transform = "translateY(0%)";
+      paragraph.style.opacity = "1";
+    });
+
+    el.addEventListener("mouseleave", () => {
+      const paragraph = el.querySelector("p");
+      paragraph.style.transform = "translateY(100%)";
+      paragraph.style.opacity = "0";
+    });
+  });
 });
