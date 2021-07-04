354cc4f247fff545e1846e0288a325e17adb42f5
330i <enactirace@gmail.com>
2021-07-03 20:04:57 -0500

    kinda finished with this except for some details

--- assets/js/projectsSwiper.js
+++ assets/js/projectsSwiper.js
@@ -2,20 +2,54 @@ document.querySelectorAll(".projectinfo").forEach(function(projectinfo) {
   projectinfo.addEventListener("mouseenter", function(event) {
     let projectnumber = projectinfo.getAttribute('projectnumber');
     document.querySelector(".project-count-h1").style.opacity = "0";
-    for(let i=1;i<7;i++) {
+    for(let i=1;i<13;i++) {
       document.querySelector(`.project-img${i}`).classList.remove(`project-img${i}-transition`);
     }
     document.querySelector(`.project-img${projectnumber}`).classList.add(`project-img${projectnumber}-transition`);
   });
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 780) {
+      projectinfo.style.width = "80vw";
+      projectinfo.style.margin = "0";
+      document.querySelector(".project-count").style.width = "80vw"
+      document.querySelector(`.project-img-div`).style.position = "relative";
+      document.querySelector(`.project-img-div`).style.width = "80vw";
+      document.querySelector(`.project-img-div`).style.left = "0";
+      document.querySelector(`.project-img-div`).style.margin = "-5vh 0 3vh 0";
+      document.querySelector('.morebtn').style.margin = "415px 0 0 -4vw"
+      for(let i=1;i<13;i++) {
+        document.querySelector(`.project-img${i}`).style.width = "80vw";
+      }
+    } else {
+      projectinfo.style.width = "40vw";
+      projectinfo.style.margin = "0 0 0 40vw";
+      document.querySelector(".project-count").style.width = "35vw"
+      document.querySelector(`.project-img-div`).style.position = "absolute";
+      document.querySelector(`.project-img-div`).style.width = "35vw";
+      document.querySelector(`.project-img-div`).style.left = "10vw";
+      document.querySelector(`.project-img-div`).style.margin = "0";
+      document.querySelector('.morebtn').style.margin = "415px 0 0 16vw"
+      for(let i=1;i<13;i++) {
+        document.querySelector(`.project-img${i}`).style.width = "35vw"
+      }
+    }
+  });
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (window.innerWidth < 780) {
-    document.querySelector(".projectinfo").style.width = "80vw";
-    document.querySelector(".projectinfo").style.margin = "0";
-  } else {
-    document.querySelector(".projectinfo").style.width = "40vw";
-    document.querySelector(".projectinfo").style.margin = "0 0 0 40vw";
+function nextPage() {
+  let page1 = document.querySelector(".projectinfodiv");
+  let page2 = document.querySelector(".projectinfodiv-next");
+  if(page1.style.opacity=="0") {
+    page1.style.opacity = "1";
+    page2.style.opacity = "0"
+    page1.style.zIndex = "2"
+    page2.style.zIndex = "1"
   }
-  window.addEventListener("resize", checkWindowSize);
-});
+  else {
+    page1.style.opacity = "0";
+    page2.style.opacity = "1"
+    page1.style.zIndex = "1"
+    page2.style.zIndex = "2"
+  }
+}
